42c015ff258f005c3de3b4aad80a2aa1e5c38c37
Omid <om.sargazi@gmail.com>
2024-02-22 02:36:24 +0330

    install axios, redux tool kit and fetch data

---
+++ redux-toolkit/src/features/productsSlice.js
@@ -0,0 +1,43 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const initialState = {
+  items: [],
+  status: null,
+  error: null,
+};
+
+export const productsFetch = createAsyncThunk(
+  "products/productsFetch",
+  async (id = null, { rejectWithValue }) => {
+    try {
+      const response = await axios.get("https://fakestoreapi.com/products");
+      console.log(response);
+      return response?.data;
+    } catch (err) {
+      return rejectWithValue(err.response.data);
+    }
+  }
+);
+
+export const productSlice = createSlice({
+  name: "products",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(productsFetch.pending, (state, action) => {
+        console.log("pending");
+        state.status = "pending";
+      })
+      .addCase(productsFetch.fulfilled, (state, action) => {
+        state.status = "success";
+        state.items = action.payload;
+      })
+      .addCase(productsFetch.rejected, (state, action) => {
+        state.status = "rejected";
+      });
+  },
+});
+
+export default productSlice.reducer;
