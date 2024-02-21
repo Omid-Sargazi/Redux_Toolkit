f1587fe47ad1377284e8cb710747af419be8ab14
Omid <om.sargazi@gmail.com>
2024-02-21 23:44:46 +0330

    build navbar and install react icons

--- redux-toolkit/src/components/Navbar.js
+++ redux-toolkit/src/components/Navbar.js
@@ -1,7 +1,22 @@
 import React from "react";
-
+import { FaShoppingBag } from "react-icons/fa";
+import { Link } from "react-router-dom";
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="nav-bar">
+      <Link to="/">
+        <h2>OmidShop</h2>
+      </Link>
+      <Link to="/cart">
+        <div className="nav-bag">
+          <FaShoppingBag size={30} />
+          <span className="bag-quantity">
+            <span>3</span>
+          </span>
+        </div>
+      </Link>
+    </nav>
+  );
 };
 
 export default Navbar;
