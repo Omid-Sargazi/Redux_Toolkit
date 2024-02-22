b024bf9f2cebe3f6ef96a6bfd5af505225394adc
Omid <om.sargazi@gmail.com>
2024-02-23 01:39:40 +0330

    build home page responsive

--- redux-toolkit/src/components/Home.js
+++ redux-toolkit/src/components/Home.js
@@ -3,7 +3,36 @@ import { useGetProductsApiByNameQuery } from "../features/productsApi";
 const Home = () => {
   const { data, error, isLoading } = useGetProductsApiByNameQuery();
 
-  return <div>{console.log(data, "homePage")}</div>;
+  return (
+    <div className="home-container">
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : error ? (
+        <p>An error occurred</p>
+      ) : (
+        <>
+          <h2>New Items</h2>
+          <div className="products">
+            {data?.map((product) => (
+              <div key={product.id} className="product">
+                <h3>{product.title.substring(0, 10)}</h3>
+                <img src={product.image} alt="product" />
+                <div className="details">
+                  <span className="des">
+                    {product.description.substring(0, 18)}
+                  </span>
+                  <span className="price">
+                    <span>Price: </span>${product.price}
+                  </span>
+                </div>
+                <button>Add To Cart</button>
+              </div>
+            ))}
+          </div>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default Home;
