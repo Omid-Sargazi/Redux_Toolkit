f62192a6572c453341e46df2383f8d229a28ccf4
Omid <om.sargazi@gmail.com>
2024-02-24 19:04:16 +0330

    build cart shopping

--- redux-toolkit/src/components/Cart.js
+++ redux-toolkit/src/components/Cart.js
@@ -1,7 +1,70 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { FaArrowCircleLeft } from "react-icons/fa";
 
 const Cart = () => {
-  return <div>Cart</div>;
+  const cart = useSelector((state) => state.cart);
+
+  return (
+    <div className="cart-container">
+      <h2>Shopping Cart</h2>
+      {cart.cartItems.length === 0 ? (
+        <div className="cart-empty">
+          <p>Your cart is currently empty.</p>
+          <div className="start-shopping">
+            <Link to="/">
+              <FaArrowCircleLeft />
+              <span>start Shopping</span>
+            </Link>
+          </div>
+        </div>
+      ) : (
+        <div>
+          <div className="titles">
+            <h3 className="product-title">Products</h3>
+            <h3 className="price">Price</h3>
+            <h3 className="Quantity">Quantity</h3>
+            <h3 className="total">Total</h3>
+          </div>
+          <div className="cart-items">
+            {cart.cartItems.map((cartItem) => (
+              <div className="cart-item" key={cartItem.id}>
+                <div className="cart-product">
+                  <img src={cartItem.image} alt={cartItem.title} />
+                  <div>
+                    <h3>{cartItem.title.substring(0, 10)}</h3>
+                    <p>{cartItem.description.substring(0, 10)}</p>
+                    <button>Remove</button>
+                  </div>
+                </div>
+                <div className="cart-product-price">${cartItem.price}</div>
+                <div className="cart-product-quantity">
+                  <button>-</button>
+                  <div className="count">{cartItem.cartQuantity}</div>
+                  <button>+</button>
+                </div>
+                <div className="cart-product-total-price">
+                  ${cartItem.price + cartItem.cartQuantity}
+                </div>
+              </div>
+            ))}
+          </div>
+          <div className="cart-summary">
+            <button className="clear-cart">Clear Cart</button>
+            <div className="cart-checkout">
+              <div className="subtotal">
+                <span>Subtotal</span>
+                <span className="amount">${cart.cartTotalAmount}</span>
+              </div>
+              <p>Taxes and Shipping calculated at checkout </p>
+              <button>Check out </button>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Cart;
